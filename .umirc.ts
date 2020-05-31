f4d9ecaf6d5f8308f3d6ce70ba0dc9e3f879084b
chenzhengqi <chenzhengqi@wisefin.tech>
2020-06-01 00:43:25 +0800

    update

---
+++ .umirc.ts
@@ -0,0 +1,29 @@
+import { IConfig } from 'umi-types';
+
+// ref: https://umijs.org/config/
+const config: IConfig =  {
+  treeShaking: true,
+  routes: require('./src/router/index'),
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: false,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'ionic_demo',
+      dll: true,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
+
+export default config;
